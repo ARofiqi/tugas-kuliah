a2cf81f972d86157679db073e6ef6ebdbcb02617
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2023-10-15 23:02:20 +0700

    praktikum2

--- praktikum1/index.js
+++ praktikum1/index.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+let a = 50;
+let b = 2;
+
+let result = (a,b) => {
+return (a > b) ? '100' : '25';
+}
+
+console.log(result (b,a));
---
+++ praktikum2/index.js
@@ -0,0 +1,21 @@
+const findNextPrime = (num) => {
+  while (++num) if ([...Array(num).keys()].slice(2).every((i) => num % i)) return num;
+};
+
+const isPrime = (num) => num > 1 && ![...Array(num - 1).keys()].slice(2).some((i) => num % i === 0);
+
+// 1
+const prompt = require("prompt-sync")();
+
+console.log("1. program untuk menampilkan output bilangan prima setelah input bilangan tertentu");
+let angka = prompt("Masukan Angka : ");
+console.log(`Hasilnya ${findNextPrime(angka)}`);
+
+console.log("2. program untuk menghitung jumlah bilangan prima pada rentang input awal sampai input akhir");
+let awal = prompt("Masukan awal : ");
+let akhir = prompt("Masukan akhir : ");
+let hasil = 0;
+for (let i = awal; i < akhir; i++) {
+  hasil += isPrime(i) ? i : 0;
+}
+console.log(`Hasilnya ${hasil}`);
