1af0aef81d03822a0b062aa841e54f0a1ca1ee87
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2023-10-24 22:11:14 +0700

    tugas4

--- praktikum4/index.js
+++ praktikum4/index.js
@@ -1,18 +1,80 @@
 let studentScore = [
-    {
-        name: "Andi",
-        score: 90
-    },
-    {
-        name: "Rudi",
-        score: 80
-    },
-    {
-        name: "Dira",
-        score: 100
-    },
-]
+  {
+    name: "Andi",
+    score: 90,
+  },
+  {
+    name: "Rudi",
+    score: 80,
+  },
+  {
+    name: "Dira",
+    score: 100,
+  },
+];
 
 console.log(`Murid dengan nilai tertinggi`);
 console.log(`Nama \t: ${studentScore[2].name}`);
 console.log(`Score \t: ${studentScore[2].score}`);
+
+var motoGP = [
+  {
+    circuit: "Losail",
+    location: "Qatar",
+    winner: {
+      firstName: "Andrea",
+      lastName: "Dovizioso",
+      country: "Italy",
+    },
+  },
+  {
+    circuit: "Autodromo",
+    location: "Argentine",
+    winner: {
+      firstName: "Cal",
+      lastName: "Crutchlow",
+      country: "UK",
+    },
+  },
+  {
+    circuit: "De Jerez",
+    location: "Spain",
+    winner: {
+      firstName: "Valentino",
+      lastName: "Rossi",
+      country: "Italy",
+    },
+  },
+  {
+    circuit: "Mugello",
+    location: "Italy",
+    winner: {
+      firstName: "Andrea",
+      lastName: "Dovizioso",
+      country: "Italy",
+    },
+  },
+];
+
+let result = {};
+
+for (let i = 0; i < motoGP.length; i++) {
+  let race = motoGP[i];
+  let winner = race.winner;
+  let country = winner.country;
+
+  if (!result[country]) {
+    result[country] = [];
+  }
+
+  result[country].push({
+    circuit: race.circuit,
+    location: race.location,
+    winner: {
+      firstName: winner.firstName,
+      lastName: winner.lastName,
+    },
+  });
+}
+
+console.log(result);
